0f58fa25f98120b46160cdb3904d3d0addad937b
payalbkasbe <payalbkasbe29@gmail.com>
2026-08-02 16:03:28 +0530

    Update guess_number.py

--- 02_guess_the_number/guess_number.py
+++ 02_guess_the_number/guess_number.py
@@ -1,6 +1,6 @@
 import random
 
-print("🎯 Welcome to Guess the Number Game!")
+print("Welcome to Guess the Number Game!")
 
 secret_number = random.randint(1, 100)
 attempts = 0
@@ -11,14 +11,14 @@ while True:
         attempts += 1
 
         if guess < secret_number:
-            print("📉 Too Low! Try Again.")
+            print("Too Low! Try Again.")
 
         elif guess > secret_number:
-            print("📈 Too High! Try Again.")
+            print("Too High! Try Again.")
 
         else:
-            print(f"🎉 Congratulations! You guessed the number in {attempts} attempts.")
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
             break
 
     except ValueError:
-        print("❌ Please enter a valid number.")
+        print("Please enter a valid number.")
